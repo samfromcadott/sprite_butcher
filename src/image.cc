c7c5525bcbcb5bd3aa682a7cf41dc532fe5eaadc
samfromcadott <samahstan@gmail.com>
2024-05-02 13:13:47 -0500

    Created an image class

--- src/image.cc
+++ src/image.cc
@@ -14,30 +14,66 @@ using namespace rectpack2D;
 static std::mt19937 gen;
 static std::uniform_int_distribution<int> dist(0, 255);
 
+export struct Color {
+	unsigned char r = 0, g = 0, b = 0, a = 0;
+};
+
+export class Image {
+private:
+	vector<Color> pixels;
+	size_t width = 0, height = 0;
+
+public:
+	Image(){}
+	Image(const size_t width, const size_t height) {
+		pixels.resize(width * height);
+		this->width = width;
+		this->height = height;
+	}
+
+	// Setter
+	Color& operator()(const size_t x, const size_t y) {
+		return pixels[ width * y + x ];
+	}
+	// Getter
+	Color operator()(const size_t x, const size_t y) const {
+		return pixels[ width * y + x ];
+	}
+
+	void save(const string& filename) {
+		std::vector<unsigned char> image;
+
+		for (auto pixel : pixels) {
+			image.push_back(pixel.r);
+			image.push_back(pixel.g);
+			image.push_back(pixel.b);
+			image.push_back(pixel.a);
+		}
+
+		lodepng::encode(filename.c_str(), image, width, height);
+	}
+};
+
 /// This function saves the sprite sheet with random colors filling the frames
 export bool save_sheet_debug(const string& filename, const rect_wh& canvas_size, const std::vector<rect_type>& frames) {
 	// Create an empty canvas
-	std::vector<unsigned char> image;
-	image.resize(canvas_size.w * canvas_size.h * 4);
+	Image canvas(canvas_size.w, canvas_size.h);
 
 	// For each frame choose a random color and draw it on the canvas
 	for (auto frame : frames) {
-		auto red = dist(gen);
-		auto green = dist(gen);
-		auto blue = dist(gen);
-		unsigned char alpha = 255;
+		unsigned char red = dist(gen);
+		unsigned char green = dist(gen);
+		unsigned char blue = dist(gen);
+		Color color = Color {red, green, blue, 255};
 
 		for (int x = frame.x; x < frame.x + frame.w; x++)
 		for (int y = frame.y; y < frame.y + frame.h; y++) {
-			image[ 4 * canvas_size.w * y + (4*x+0) ] = red;
-			image[ 4 * canvas_size.w * y + (4*x+1) ] = green;
-			image[ 4 * canvas_size.w * y + (4*x+2) ] = blue;
-			image[ 4 * canvas_size.w * y + (4*x+3) ] = alpha;
+			canvas(x, y) = color;
 		}
 	}
 
 	// Save the canvas
-	lodepng::encode(filename.c_str(), image, canvas_size.w, canvas_size.h);
+	canvas.save(filename);
 
 	return true;
 }
