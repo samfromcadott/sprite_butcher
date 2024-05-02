4bc986dc94e24269573829ee1c22b4116408eff9
samfromcadott <samahstan@gmail.com>
2024-05-01 20:07:26 -0500

    Moved packing to the `packing` module

---
+++ src/packing.cc
@@ -0,0 +1,37 @@
+#include "finders_interface.h"
+
+export module packing;
+
+using namespace rectpack2D;
+
+constexpr bool allow_flip = true;
+const auto runtime_flipping_mode = flipping_option::ENABLED;
+export using spaces_type = rectpack2D::empty_spaces<allow_flip, default_empty_spaces>;
+export using rect_type = output_rect_t<spaces_type>;
+export using rect_array = std::vector<rect_type>;
+
+auto report_successful = [](rect_type&) {
+	return callback_result::CONTINUE_PACKING;
+};
+
+auto report_unsuccessful = [](rect_type&) {
+	return callback_result::ABORT_PACKING;
+};
+
+const auto max_side = 1000;
+const auto discard_step = -4;
+
+export std::tuple< rect_wh, std::vector<rect_type> > pack_frames(std::vector<rect_type> rectangles) {
+	const rect_wh canvas_size = find_best_packing<spaces_type>(
+		rectangles,
+		make_finder_input(
+			max_side,
+			discard_step,
+			report_successful,
+			report_unsuccessful,
+			runtime_flipping_mode
+		)
+	);
+
+	return {canvas_size, rectangles};
+}
